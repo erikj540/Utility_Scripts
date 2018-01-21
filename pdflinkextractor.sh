0676cd3ac3fb82e1218e02a39f3138a728772353
erikj540 <erik.k.johnson@colorado.edu>
2018-01-21 13:23:27 -0700

    Added script (pdflinkextractor.sh) which gets all pdfs linked to from a website

---
+++ pdflinkextractor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# NAME:         pdflinkextractor
+# AUTHOR:       Glutanimate (http://askubuntu.com/users/81372/), 2013
+# LICENSE:      GNU GPL v2
+# DEPENDENCIES: wget lynx
+# DESCRIPTION:  extracts PDF links from websites and dumps them to the stdout and as a textfile
+#               only works for links pointing to files with the ".pdf" extension
+#
+# USAGE:        pdflinkextractor "www.website.com"
+
+WEBSITE="$1"
+
+echo "Getting link list..."
+echo "$1"
+echo "$2"
+
+lynx -cache=0 -dump -listonly "$WEBSITE" | grep ".*\.pdf$" | awk '{print $2}' | tee pdflinks.txt
+
+# OPTIONAL
+
+# DOWNLOAD PDF FILES
+
+echo "Downloading..."    
+wget -P "$2"/ -i pdflinks.txt
+rm pdflinks.txt
